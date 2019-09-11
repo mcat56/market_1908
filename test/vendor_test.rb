b4bcd839a5949255775197bceeb795a5a150dd61
Mary Lang <mcl86@cornell.edu>
2019-09-11 13:12:23 -0600

    Complete Iteration1

---
+++ test/vendor_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < MiniTest::Test
+
+  def setup
+     @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_initialize
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_check_stock
+    assert_equal 0, @vendor.check_stock("Peaches")
+  end
+
+  def test_stock
+    @vendor.stock("Peaches", 30)
+
+    assert_equal 30, @vendor.check_stock("Peaches")
+  end
+
+
+
+end
